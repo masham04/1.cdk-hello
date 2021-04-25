528b1edef4260b62352596817953eed8f674d036
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-04-25 14:56:31 +0500

    new

--- bin/cdk-hello.ts
+++ bin/cdk-hello.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkHelloStack } from '../lib/cdk-hello-stack';
+import { CdkHelloWorld } from '../lib/cdk-hello-stack';
 
 const app = new cdk.App();
-new CdkHelloStack(app, 'CdkHelloStack', {
+new CdkHelloWorld(app, 'CdkHelloWorld', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
